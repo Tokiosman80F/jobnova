2b9913d0c205a6b7bc72f3a2e594ca896f060831
Tokiosman80F <tokiosman96@gmail.com>
2024-11-15 09:46:57 +0600

    feat:add error page with 404 message - Created a responsive Error page - Added Back to home button for easy navigation

---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import error from "../assets/images/error/error_image.png";
+import logo from "../assets/images/error/logo__error.png";
+
+const Error = () => {
+  return (
+    <div className="flex flex-col items-center justify-evenly h-screen">
+      {/* Logo */}
+      <img src={logo} alt="Logo" />
+
+      {/* Error Message Section */}
+      <div className="flex flex-col items-center gap-4">
+        <img src={error} alt="Error" />
+        <h1 className="uppercase font-bold text-3xl">Page Not Found</h1>
+        <p className="text-gray-500">Whoops, this is embarrassing.</p>
+        <p className="text-gray-500">
+          Looks like the page you were looking for wasn't found.
+        </p>
+        {/* Back to Home Link */}
+        <p>
+          <Link to="/" className="uppercase text-blue-600 hover:underline">
+            Back to Home
+          </Link>
+        </p>
+      </div>
+
+      {/* Copyright Text */}
+      <p className="mb-4 md:mb-0 text-gray-500">
+        © {new Date().getFullYear()} Developed by Developer
+      </p>
+    </div>
+  );
+};
+
+export default Error;
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Error from "../pages/Error";
 import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
